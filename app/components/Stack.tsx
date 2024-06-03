973438e7b173742725d1a59c627a3eb73f133092
Kisuyo <ig.intr3st@gmail.com>
2024-06-03 16:18:42 +0200

    mobile

---
+++ app/components/Stack.tsx
@@ -0,0 +1,72 @@
+import { motion } from "framer-motion";
+
+export default function Stack(props: { scrollPointReached: number }) {
+  const StackVariants = {
+    expanded: { height: "400px" },
+    closed: { height: "0px" },
+  };
+  return (
+    <motion.div
+      variants={StackVariants}
+      layout
+      initial={closed}
+      transition={{ duration: 0.8, easings: "ease-out" }}
+      animate={3 <= props.scrollPointReached ? "expanded" : "closed"}
+      className="w-full h-full flex md:flex-row flex-col gap-[20px] overflow-hidden"
+    >
+      <div className="w-full h-full flex flex-col gap-[20px]">
+        <div className="bg-[#0e0e0e] rounded-[20px] md:h-1/2 h-[100px]"></div>
+        <div className="bg-[#0e0e0e] rounded-[20px] md:h-1/2 h-[100px] font-bold text-white/30  flex items-center justify-evenly">
+          <a
+            href="https://github.com/Kisuyo"
+            target="_blank"
+            className="hover:text-white/40 cursor-pointer transition-all"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.instagram.com/kisuyot_t/"
+            target="_blank"
+            className="hover:text-white/40 cursor-pointer transition-all"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://x.com/KisuyoTT"
+            target="_blank"
+            className="hover:text-white/40 cursor-pointer transition-all"
+          >
+            Twitter
+          </a>
+        </div>
+      </div>
+      <div className="w-full h-full bg-[#0e0e0e] p-2 rounded-[20px] flex justify-between flex-col">
+        <a
+          href="https://github.com/learn-anything/learn-anything.xyz"
+          target="_blank"
+          className="flex items-center font-light w-full gap-4 hover:bg-[#121212] transition-all p-2 rounded-md cursor-pointer"
+        >
+          <div className="min-w-[100px] h-[100px] bg-[#171717] rounded-md"></div>
+          <div className="text-white/60 pr-[50px] ">
+            <div className="text-[20px] font-bold text-white">
+              Learn Anything
+            </div>
+            It is a app to track your links and learn new topics fast.
+          </div>
+        </a>
+        <a
+          href="https://github.com/kuskusapp/kuskus"
+          target="_black"
+          className="flex items-center w-full gap-4 p-2 rounded-md cursor-pointer hover:bg-[#121212] transition-all"
+        >
+          <div className="text-white/60 pl-[50px] text-end">
+            <div className="text-[20px] font-bold text-white">Kuskus</div>
+            Its an app that allows you to share places you have eaten in like
+            restaurants or cafes.
+          </div>
+          <div className="min-w-[100px] h-[100px] bg-[#171717] rounded-md"></div>
+        </a>
+      </div>
+    </motion.div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import Stack from "./components/Stack";
+import StackMobile from "./components/StackMobile";
 
 export default function Home() {
   const [scrollPointReached, setScrollPointReached] = useState(0);
+  const [showMobile, setShowMobile] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const scrollPoint1 = 500;
       const scrollPoint2 = 1200;
       const scrollPoint3 = 1900;
+
       if (scrollPointReached !== 0 && scrollPoint1 >= scrollPosition) {
         setScrollPointReached(0);
       }
@@ -25,9 +29,18 @@ export default function Home() {
         setScrollPointReached(3);
       }
     };
-
+    const handleResize = () => {
+      if (768 >= window.innerWidth) {
+        setShowMobile(true);
+      } else {
+        setShowMobile(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
     window.addEventListener("scroll", handleScroll);
+    handleResize();
   });
+
   const ParentVariants = {
     expanded: { padding: "20px" },
     closed: { padding: "0px" },
@@ -36,20 +49,27 @@ export default function Home() {
     expanded: {
       border: "1px solid rgba(255,255,255,0.15)",
       borderRadius: "20px",
+      fontSize: `${showMobile ? "50px" : "76px"}`,
     },
     closed: {
       border: "none",
       borderRadius: "0px",
+      fontSize: "76px",
     },
   };
   const AboutMeVariants = {
-    expanded: { width: "400px", display: "flex" },
-    closed: { width: "0px", display: "none" },
+    expanded: {
+      width: `${showMobile ? "100%" : "400px"}`,
+      display: "flex",
+      height: "100%",
+    },
+    closed: {
+      width: `${showMobile ? "100%" : "0px"}`,
+      display: "none",
+      height: `${showMobile ? "0px" : "100%"}`,
+    },
   };
-  const StackVariants = {
-    expanded: { height: "400px" },
-    closed: { height: "0px" },
-  };
+
   const TitleVariants = {
     expanded: {},
     closed: {},
@@ -66,6 +86,7 @@ export default function Home() {
           <motion.div
             variants={ParentVariants}
             transition={{ duration: 1, easings: "ease-out" }}
+            initial={closed}
             animate={1 <= scrollPointReached ? "expanded" : "closed"}
             className="h-screen sticky top-0 flex md:flex-row flex-col gap-[20px] left-0 w-full"
           >
@@ -73,6 +94,7 @@ export default function Home() {
               <motion.div
                 variants={HeroWrapperVariants}
                 transition={{ duration: 0.2, easings: "ease-out" }}
+                initial={closed}
                 layout
                 animate={1 <= scrollPointReached ? "expanded" : "closed"}
                 className=" border flex items-center relative justify-center flex-col w-full h-full"
@@ -81,8 +103,9 @@ export default function Home() {
                   variants={TitleVariants}
                   transition={{ duration: 1, easings: "ease-out" }}
                   layout
+                  initial={closed}
                   animate={1 <= scrollPointReached ? "expanded" : "closed"}
-                  className="text-[76px]   font-bold"
+                  className=" font-bold"
                 >
                   Kisuyo
                 </motion.div>
@@ -90,88 +113,35 @@ export default function Home() {
                   variants={DescriptionVariants}
                   transition={{ duration: 1, easings: "ease-out" }}
                   layout
+                  initial={closed}
                   animate={1 <= scrollPointReached ? "expanded" : "closed"}
-                  className="text-[20px] font-normal text-white/50 w-[50%] text-center"
+                  className="md:text-[20px] text-[16px] font-normal text-white/50 md:w-[50%] w-[80%] text-center"
                 >
                   I am a front-end developer skilled in React and Tailwind.
                 </motion.div>
               </motion.div>
-              <motion.div
-                variants={StackVariants}
-                layout
-                transition={{ duration: 0.8, easings: "ease-out" }}
-                animate={3 <= scrollPointReached ? "expanded" : "closed"}
-                className="w-full h-full flex gap-[20px] overflow-hidden"
-              >
-                <div className="w-full h-full flex flex-col gap-[20px]">
-                  <div className="bg-[#0e0e0e] rounded-[20px] h-1/2"></div>
-                  <div className="bg-[#0e0e0e] rounded-[20px] h-1/2 font-bold text-white/30  flex items-center justify-evenly">
-                    <a
-                      href="https://github.com/Kisuyo"
-                      target="_blank"
-                      className="hover:text-white/40 cursor-pointer transition-all"
-                    >
-                      Github
-                    </a>
-                    <a
-                      href="https://www.instagram.com/kisuyot_t/"
-                      target="_blank"
-                      className="hover:text-white/40 cursor-pointer transition-all"
-                    >
-                      Instagram
-                    </a>
-                    <a
-                      href="https://x.com/KisuyoTT"
-                      target="_blank"
-                      className="hover:text-white/40 cursor-pointer transition-all"
-                    >
-                      Twitter
-                    </a>
-                  </div>
-                </div>
-                <div className="w-full h-full bg-[#0e0e0e] p-2 rounded-[20px] flex justify-between flex-col">
-                  <a
-                    href="https://github.com/learn-anything/learn-anything.xyz"
-                    target="_blank"
-                    className="flex items-center font-light w-full gap-4 hover:bg-[#121212] transition-all p-2 rounded-md cursor-pointer"
-                  >
-                    <div className="min-w-[100px] h-[100px] bg-[#171717] rounded-md"></div>
-                    <div className="text-white/60 pr-[50px] ">
-                      <div className="text-[20px] font-bold text-white">
-                        Learn Anything
-                      </div>
-                      It is a app to track your links and learn new topics fast.
-                    </div>
-                  </a>
-                  <a
-                    href="https://github.com/kuskusapp/kuskus"
-                    target="_black"
-                    className="flex items-center w-full gap-4 p-2 rounded-md cursor-pointer hover:bg-[#121212] transition-all"
-                  >
-                    <div className="text-white/60 pl-[50px] text-end">
-                      <div className="text-[20px] font-bold text-white">
-                        Kuskus
-                      </div>
-                      It is a app to track your links and learn new topics fast.
-                    </div>
-                    <div className="min-w-[100px] h-[100px] bg-[#171717] rounded-md"></div>
-                  </a>
-                </div>
-              </motion.div>
+
+              {!showMobile ? (
+                <Stack scrollPointReached={scrollPointReached} />
+              ) : null}
             </div>
+
             <motion.div
               variants={AboutMeVariants}
               layout
+              initial={closed}
               transition={{ duration: 0.8, easings: "ease-out" }}
               animate={2 <= scrollPointReached ? "expanded" : "closed"}
               className="text-white text-[14px] rounded-[20px] bg-[#0e0e0e] overflow-hidden"
             >
               <div className="min-w-[300px] w-full">
-                <img
-                  src="/pfp.jpg"
-                  className="w-full h-fit rounded-[20px]"
-                  alt="pfp"
-                />
+                {!showMobile ? (
+                  <img
+                    src="/pfp.jpg"
+                    className="w-full max-h-fit h-fit rounded-[20px]"
+                    alt="pfp"
+                  />
+                ) : null}
                 <div className="p-5 text-[14px] flex flex-col gap-3 font-light">
                   <div className="text-white/60 pr-[20px]">
                     <div className="text-white font-bold">
@@ -184,17 +154,76 @@ export default function Home() {
                   </div>
                   <div>
                     🎮{" "}
-                    <span className="text-white/60">- I play Video games.</span>
+                    <span className="text-white/60">- I play Video Games.</span>
                   </div>
                   <div>
-                    🍳 <span className="text-white/60">- I like cooking.</span>
+                    🍳 <span className="text-white/60">- I like Cooking.</span>
                   </div>
                 </div>
               </div>
             </motion.div>
           </motion.div>
         </div>
-        <div className=""></div>
+        {showMobile ? (
+          <div className="p-[20px] pt-[0px] h-full">
+            <motion.div className="w-full h-full flex md:flex-row flex-col gap-[20px] overflow-hidden">
+              <div className="bg-[#0e0e0e] rounded-[20px] md:h-1/2 h-[100px]"></div>
+
+              <div className="w-full h-full bg-[#0e0e0e] p-2 rounded-[20px] flex justify-between flex-col">
+                <a
+                  href="https://github.com/learn-anything/learn-anything.xyz"
+                  target="_blank"
+                  className="flex md:flex-row flex-col md:items-center font-light w-full gap-4 hover:bg-[#121212] transition-all p-2 rounded-md cursor-pointer"
+                >
+                  <div className="w-[100px] h-[100px] bg-[#171717] rounded-md"></div>
+                  <div className="text-white/60 pr-[50px] ">
+                    <div className="text-[20px] font-bold text-white">
+                      Learn Anything
+                    </div>
+                    It is a app to track your links and learn new topics fast.
+                  </div>
+                </a>
+                <a
+                  href="https://github.com/kuskusapp/kuskus"
+                  target="_black"
+                  className="flex md:flex-row flex-col md:items-center w-full gap-4 p-2 rounded-md cursor-pointer hover:bg-[#121212] transition-all"
+                >
+                  <div className="w-[100px] h-[100px] bg-[#171717] rounded-md"></div>
+                  <div className="text-white/60">
+                    <div className="text-[20px] font-bold text-white">
+                      Kuskus
+                    </div>
+                    Its an app that allows you to share places you have eaten in
+                    like restaurants or cafes.
+                  </div>
+                </a>
+              </div>
+              <div className="bg-[#0e0e0e] rounded-[20px] md:h-1/2 h-[100px] font-bold text-white/30  flex items-center justify-evenly">
+                <a
+                  href="https://github.com/Kisuyo"
+                  target="_blank"
+                  className="hover:text-white/40 cursor-pointer transition-all"
+                >
+                  Github
+                </a>
+                <a
+                  href="https://www.instagram.com/kisuyot_t/"
+                  target="_blank"
+                  className="hover:text-white/40 cursor-pointer transition-all"
+                >
+                  Instagram
+                </a>
+                <a
+                  href="https://x.com/KisuyoTT"
+                  target="_blank"
+                  className="hover:text-white/40 cursor-pointer transition-all"
+                >
+                  Twitter
+                </a>
+              </div>
+            </motion.div>
+          </div>
+        ) : null}
       </div>
     </>
   );
