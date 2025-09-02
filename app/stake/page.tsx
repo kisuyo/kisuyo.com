42644729c88e6d778e1cfe8ed6db9ea7e8bdd788
Kisuyo <ig.intr3st@gmail.com>
2025-09-02 18:59:09 +0200

    .

--- app/stake/page.tsx
+++ app/stake/page.tsx
@@ -78,130 +78,132 @@ export default function Stake() {
   const handleMouseUp = (e: React.MouseEvent) => {};
   return (
     <>
-      <div
-        className="max-w-[560px] mx-auto bg-[var(--secondary-900)] h-screen flex flex-col"
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
-      >
-        <motion.div
-          animate={{
-            y: [-300, 0],
-          }}
-          transition={{
-            duration: 1,
-            ease: "easeOut",
-          }}
-          className="h-[50%] flex justify-center items-between flex-col w-ful rounded-b-[50px]"
-          style={{
-            background: `radial-gradient(
+      <div className="w-full bg-[var(--secondary-900)]">
+        <div
+          className="max-w-[500px] mx-auto bg-[var(--secondary-900)] h-screen flex flex-col"
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        >
+          <motion.div
+            animate={{
+              y: [-300, 0],
+            }}
+            transition={{
+              duration: 1,
+              ease: "easeOut",
+            }}
+            className="h-[50%] flex justify-center items-between flex-col w-ful rounded-b-[50px]"
+            style={{
+              background: `radial-gradient(
               circle at 50% -100%,
               hsl(from var(--secondary-400) h s l / 1),
               hsl(from var(--secondary-500) h s l / 0.5)
             )
             padding-box`,
-          }}
-        >
-          <motion.div
-            animate={{
-              opacity: [0, 1],
-              filter: ["blur(10px)", "blur(0px)"],
             }}
-            transition={{
-              duration: 1,
-              ease: "easeOut",
-              delay: 1,
-            }}
-            className="  flex-1 flex items-center justify-center flex-col gap-8"
           >
-            <div className="text-[var(--primary-200)] flex items-center justify-center flex-col text-[24px] leading-[1.2] text-center">
-              <span className="font-medium">Stakes</span>
-              <span className="text-[var(--primary-200)] text-[56px] font-bold flex items-center gap-2">
-                <AnimatedCounter
-                  value={stakes[chosedStake].small}
-                  duration={1.5}
-                />
-                /
-                <AnimatedCounter
-                  value={stakes[chosedStake].big}
-                  duration={1.5}
-                />
-              </span>
-            </div>
-            <div className="text-[var(--primary-200)] flex items-center justify-center flex-col text-[24px] leading-[1.2] text-center">
-              <span className="font-medium">Buy-in</span>
-              <span className="text-[var(--primary-200)] text-[56px] font-bold flex items-center gap-2">
-                <AnimatedCounter
-                  value={stakes[chosedStake].buyIn.min}
-                  format="currency"
-                  duration={1.5}
-                />
-                /
-                <AnimatedCounter
-                  value={stakes[chosedStake].buyIn.max}
-                  format="currency"
-                  duration={1.5}
-                />
-              </span>
-            </div>
+            <motion.div
+              animate={{
+                opacity: [0, 1],
+                filter: ["blur(10px)", "blur(0px)"],
+              }}
+              transition={{
+                duration: 1,
+                ease: "easeOut",
+                delay: 1,
+              }}
+              className="  flex-1 flex items-center justify-center flex-col gap-8"
+            >
+              <div className="text-[var(--primary-200)] flex items-center justify-center flex-col text-[24px] leading-[1.2] text-center">
+                <span className="font-medium">Stakes</span>
+                <span className="text-[var(--primary-200)] text-[56px] font-bold flex items-center gap-2">
+                  <AnimatedCounter
+                    value={stakes[chosedStake].small}
+                    duration={1.5}
+                  />
+                  /
+                  <AnimatedCounter
+                    value={stakes[chosedStake].big}
+                    duration={1.5}
+                  />
+                </span>
+              </div>
+              <div className="text-[var(--primary-200)] flex items-center justify-center flex-col text-[24px] leading-[1.2] text-center">
+                <span className="font-medium">Buy-in</span>
+                <span className="text-[var(--primary-200)] text-[56px] font-bold flex items-center gap-2">
+                  <AnimatedCounter
+                    value={stakes[chosedStake].buyIn.min}
+                    format="currency"
+                    duration={1.5}
+                  />
+                  /
+                  <AnimatedCounter
+                    value={stakes[chosedStake].buyIn.max}
+                    format="currency"
+                    duration={1.5}
+                  />
+                </span>
+              </div>
 
-            <div className="text-[var(--primary-200)] text-center"></div>
+              <div className="text-[var(--primary-200)] text-center"></div>
+            </motion.div>
+            <motion.div
+              animate={{
+                opacity: [0, 1],
+                filter: ["blur(10px)", "blur(0px)"],
+              }}
+              transition={{
+                duration: 1,
+                ease: "easeOut",
+                delay: 1,
+              }}
+              className="flex justify-evenly items-end p-[20px] py-[40px] h-[100px]"
+            >
+              {stakes.map((stake, index) => {
+                const distance = Math.abs(index - chosedStake);
+                const opacity =
+                  distance === 0
+                    ? 1
+                    : distance === 1
+                    ? 0.6
+                    : distance === 2
+                    ? 0.3
+                    : 0.1;
+
+                return (
+                  <motion.div
+                    key={index}
+                    className="w-[4px] bg-white rounded-full cursor-pointer"
+                    animate={{
+                      height: chosedStake === index ? "70px" : "50px",
+                      opacity: opacity,
+                    }}
+                    transition={{
+                      duration: 0.3,
+                      ease: "easeInOut",
+                    }}
+                    onClick={() => setChosedStake(index)}
+                  ></motion.div>
+                );
+              })}
+            </motion.div>
           </motion.div>
-          <motion.div
-            animate={{
-              opacity: [0, 1],
-              filter: ["blur(10px)", "blur(0px)"],
+          <div
+            className="flex-1 w-[60%] text-center mx-auto flex items-center justify-center"
+            style={{
+              background: `linear-gradient(to bottom, var(--primary-200), transparent)`,
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+              backgroundClip: "text",
+              opacity: 0.3,
             }}
-            transition={{
-              duration: 1,
-              ease: "easeOut",
-              delay: 1,
-            }}
-            className="flex justify-evenly items-end p-[20px] py-[40px] h-[100px]"
           >
-            {stakes.map((stake, index) => {
-              const distance = Math.abs(index - chosedStake);
-              const opacity =
-                distance === 0
-                  ? 1
-                  : distance === 1
-                  ? 0.6
-                  : distance === 2
-                  ? 0.3
-                  : 0.1;
-
-              return (
-                <motion.div
-                  key={index}
-                  className="w-[4px] bg-white rounded-full cursor-pointer"
-                  animate={{
-                    height: chosedStake === index ? "70px" : "50px",
-                    opacity: opacity,
-                  }}
-                  transition={{
-                    duration: 0.3,
-                    ease: "easeInOut",
-                  }}
-                  onClick={() => setChosedStake(index)}
-                ></motion.div>
-              );
-            })}
-          </motion.div>
-        </motion.div>
-        <div
-          className="flex-1 w-[60%] text-center mx-auto flex items-center justify-center"
-          style={{
-            background: `linear-gradient(to bottom, var(--primary-200), transparent)`,
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent",
-            backgroundClip: "text",
-            opacity: 0.3,
-          }}
-        >
-          Swipe your mouse or touch the screen to select your stake
+            Swipe your mouse or touch the screen to select your stake
+          </div>
         </div>
       </div>
     </>
